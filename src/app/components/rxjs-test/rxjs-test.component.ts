51c42e02a8a8d9745d0633edec0d3702bea82566
leskovitaliy <lesko.vitaliy90@gmail.com>
2018-03-27 14:37:43 +0300

    add ngrxTest component

---
+++ src/app/components/rxjs-test/rxjs-test.component.ts
@@ -0,0 +1,58 @@
+import {Component, OnInit} from '@angular/core';
+
+import {Subscription} from 'rxjs/Subscription';
+
+import {map} from 'rxjs/operators';
+import {timer} from 'rxjs/observable/timer';
+import {from} from 'rxjs/observable/from';
+
+
+@Component({
+  selector: 'app-rxjs-test',
+  templateUrl: './rxjs-test.component.html',
+  styleUrls: ['./rxjs-test.component.css']
+})
+export class RxjsTestComponent implements OnInit {
+
+
+  private _checkStatusSub: Subscription;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    /*const source = new Observable((observer) => {
+      setInterval(() => {
+        observer.next(count++);
+        // observer.complete();
+      }, 1500);
+    });
+
+    const subject = source.subscribe(
+      (value) => {
+        console.log('next: ', value);
+      },
+      (err) => {
+        console.log('err: ', err);
+      },
+      () => {
+        console.log('completed');
+      }
+    );*/
+
+    const sourceTimer = timer(1000, 1000);
+    sourceTimer
+      .pipe(
+        map((count: any) => ++count)
+      )
+      .subscribe(value => {
+        console.log(value);
+      });
+
+    /*const source = from([1, 2, 3, 4, 5]);
+    source.pipe(
+      map(val => val + 10)
+    ).subscribe(val => console.log(val));*/
+  }
+
+}
